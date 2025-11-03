167d986bff9990b5b16d5aa732beb549f626f17f
nikhilrajpoot21 <nikhil.rajpoot.2104@gmail.com>
2025-11-03 23:08:41 +0530

    Setup the Project

---
+++ trimm/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+        <div className='bg-blue-500 p-4 text-white'>
+      <h1 className='text-2xl font-bold'>Welcome to My React App</h1>
+      <p className='mt-2'>Building modern UIs with Tailwind CSS and React.</p>
+      <button className='mt-4 bg-green-500 p-2 text-white hover:bg-green-600'>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
